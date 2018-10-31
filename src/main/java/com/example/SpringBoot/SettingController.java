80c5a4be0262784bb8a40c53e8cbcf479f3443b0
Andy Su <andy.su@dematic.com>
2018-10-31 17:12:53 -0400

    Cleaned up code

--- src/main/java/com/example/SpringBoot/SettingController.java
+++ src/main/java/com/example/SpringBoot/SettingController.java
@@ -1,4 +1,4 @@
-package com.example.SpringBoot.Controllers;
+package com.example.SpringBoot;
 
 import com.example.SpringBoot.Error.MessageKey;
 import com.example.SpringBoot.Services.DataStoreService;
@@ -10,7 +10,6 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,9 +27,9 @@ import java.util.Iterator;
 
 @RestController
 @Component
-public class Setting {
+public class SettingController {
 
-    private Logger log = LoggerFactory.getLogger(Setting.class);
+    private Logger log = LoggerFactory.getLogger(SettingController.class);
 
     private final String SETTING_KIND = "setting";
 
