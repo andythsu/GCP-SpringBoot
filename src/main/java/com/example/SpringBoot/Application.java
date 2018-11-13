80a639b4e12022bede1588051a7bc488c2efda1a
Andy Su <andy.su@dematic.com>
2018-11-13 14:35:35 -0500

    added home page

--- src/main/java/com/example/SpringBoot/Application.java
+++ src/main/java/com/example/SpringBoot/Application.java
@@ -3,11 +3,20 @@ package com.example.SpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @SpringBootApplication(scanBasePackages = {"com.example.SpringBoot"})
+@RestController
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@RequestMapping("/")
+	public String welcome(){
+		return "hello world";
+	}
 }
+
