9d1714182a0bf6049c167e63cdfc439383caf235
Andy Su <f2280c@gmail.com>
2018-11-30 18:30:45 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-# dockerCmd="docker run -it -d -e GOOGLE_CLOUD_PROJECT='andyproject-220200' -p 80:80 --name gcp -t gcp"
-# echo "docker cmd: " $dockerCmd
-# $dockerCmd
-
-docker run -it -d -e GOOGLE_CLOUD_PROJECT='andyproject-220200' -e GOOGLE_APPLICATION_CREDENTIALS='/tmp/GCP_service_account.json' -p 80:80 --name gcp -t gcp
+dockerCmd="docker run -it -d -e GOOGLE_CLOUD_PROJECT='andyproject-220200' -e GOOGLE_APPLICATION_CREDENTIALS='GCP_service_account.json' -p 80:80 --name gcp -t gcp"
+echo "docker cmd: " $dockerCmd
+$dockerCmd
